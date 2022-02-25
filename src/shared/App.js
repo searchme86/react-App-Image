a8546594c46bd689a8aedb82e5c0bd74645bec9f
searchme86 <searchme86@gmail.com>
2022-02-25 16:06:05 +0900

    ⚡️ components modified

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 import PostList from '../pages/PostList';
+import Header from '../components/Header';
+import Signup from '../pages/Signup';
+import { Grid } from '../elements';
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<PostList />} />
-        </Routes>
-      </BrowserRouter>
+      <Grid>
+        <Header></Header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PostList />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      </Grid>
     </>
   );
 }
